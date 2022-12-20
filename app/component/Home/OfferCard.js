5c0ac7d8619fa231edda3b246e05cea10ee7e79c
coderHasan <skmdmehedihasan420@gmail.com>
2022-12-20 20:40:45 +0600

    Feauture brand

--- app/component/Home/OfferCard.js
+++ app/component/Home/OfferCard.js
@@ -3,10 +3,10 @@ import React from 'react'
 
 const OfferCard = ({ img, category, title }) => {
     return (
-        <div className="!min-w-[350px] flex flex-col items-start cursor-pointer">
+        <div className="group !min-w-[350px] flex flex-col items-start cursor-pointer">
             <img className='rounded-lg' src={`/images/Future_brande/homepage-new-brand-img-${img}.webp`} alt="" />
             <h4 className="text-color text-lg font-semibold leading-6 mt-4">{category}</h4>
-            <span className="text-text_color text-xl font-bold">{title}</span>
+            <span className="text-text_color text-xl font-bold group-hover:text-primary transition-all duration-300">{title}</span>
         </div>
     )
 }
